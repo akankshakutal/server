c882f04f97802f841e314a59e088327630e97a3f
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-04-16 18:24:21 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const Express = require("express");
 
-const app = express();
+const app = Express();
 const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get("/hello", (req, res) => {
+  res.send("Hello");
 });
+
+app.use(Express.static("hello-world/build"));
 
 app.listen(port, () =>
   console.log("Express server is running on localhost:8000")
